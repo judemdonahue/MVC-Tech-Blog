51c974cad1913632b1d9ad227699188bee59262a
Jude Donahue <judedonahue@Judes-MacBook-Air.local>
2023-07-09 19:56:10 -0500

    Setup HBRs, server.js/localhostport, and package.json script setup

--- controllers/htmlroutes.js
+++ controllers/htmlroutes.js
@@ -1,1 +1,32 @@
-//swagethy jonesborg
+const router = require('express').Router();
+
+const posts = [
+    {
+        id: 1,
+        title: "Why MVC is so important",
+        user: "Juder",
+        body: "MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic."
+    },
+    {
+        id: 2,
+        title: "Authentication vs Authorization",
+        user: "Craig Xen",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat molestias aspernatur quam dolor officiis, perferendis natus optio! Totam quibusdam, praesentium ullam quae excepturi omnis, quia sunt, quos eum voluptatum sed."
+    },
+    {
+        id: 3,
+        title: "ORM",
+        user: "Squiggle Squaggle",
+        body: "Iridoseclitus erie erie my name is squiggle squiggle"
+    }
+]
+
+// GET route for rendering the Main page template
+router.get('/', async (req, res) => {
+
+    //Rendering individual post template
+    res.render('individualpost');
+
+});
+
+module.exports = router;
--- models/post.js
+++ models/post.js
@@ -5,7 +5,9 @@ class Post extends Model {}
 
 Post.init(
     {
+        id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
         title: { type: DataTypes.STRING, allowNull: false },
+        user: { type: DataTypes.STRING, allowNull: false },
         body: { type: DataTypes.STRING, allowNull: false }
     },
     {
